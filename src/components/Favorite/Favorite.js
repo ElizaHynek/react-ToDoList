f561fc2911015f0d590809e7f75361d551d352fe
Eliza Hynek <eliza.hynek@gmail.com>
2023-03-22 17:02:54 +0100

    add active-class to links

--- src/components/Favorite/Favorite.js
+++ src/components/Favorite/Favorite.js
@@ -1,9 +1,8 @@
 import PageTitle from '../PageTitle/PageTitle';
-import styles from './Favorite.module.scss';
 
 const Favorite = () => {
   return (
-    <div className={styles.favorite} >
+    <div>
       <PageTitle>Favorite</PageTitle>
       <p>Lorem ipsum</p> 
     </div>
